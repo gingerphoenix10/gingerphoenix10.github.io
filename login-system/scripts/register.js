176228f37f11abf4729a5c969b5815d68a5168b8
gingerphoenix10 <49760080+gingerphoenix10@users.noreply.github.com>
2023-05-13 20:57:14 +0100

    delete unneeded files

--- login-system/scripts/register.js
+++
@@ -1,6 +0,0 @@
-if (typeof(Storage) !== "undefined") {
-        localStorage.setItem("username", "gingerphoenix10");
-        localStorage.setItem("password", "haha no");
-    } else {
-        console.log("Sorry! No Web Storage support..")
-    }
